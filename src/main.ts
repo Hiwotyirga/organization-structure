fea2c313a69b2bdded61c190d3592aee2df4d763
Hiwot yirga <hiwotyirga30@gmail.com>
2024-08-01 12:04:00 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Enable CORS with configuration options
+  app.enableCors({
+    origin: 'http://localhost:8080', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // Allow credentials
+  });
+
+  await app.listen(9000);
 }
 bootstrap();
